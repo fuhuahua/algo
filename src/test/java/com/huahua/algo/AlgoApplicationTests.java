961cb0ccf329132d46f612a205a0598ab428e6d2
huxingkong <huxingkong2012@163.com>
2020-07-20 16:07:38 +0800

    project init

---
+++ src/test/java/com/huahua/algo/AlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huahua.algo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
